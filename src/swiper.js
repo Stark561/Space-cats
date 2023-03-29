116e7e3fc4ccb63282d1be9eb0342b520514f1ff
Eleonora907 <elenatroyan2010@ukr.net>
2023-03-29 20:47:59 +0200

    Updated swiper.js

--- src/swiper.js
+++ src/swiper.js
@@ -1,16 +1,10 @@
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   spaceBetween: 18,
-  // Optional parameters
-  // direction: 'horizontal',
   loop: true,
   slidesToScroll: 1,
   speed: 800,
-//   lazy: {
-// loadPrevNext: true,
-//   }
   slideToClickedSlide: true,
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
@@ -18,14 +12,5 @@ const swiper = new Swiper('.swiper', {
   },
   autoplay: {
     delay: 5000,
-  }
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
+  },
 });
