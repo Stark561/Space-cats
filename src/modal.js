d649977f9da2f1333daeb7d2c7ae390d58a4f3bc
Vladislavv99 <vladlegur6@gmail.com>
2023-03-30 13:03:27 +0300

    fix

--- src/modal.js
+++ src/modal.js
@@ -1,14 +1,12 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const refs = {
+  openModalBtn: document.querySelector('[data-subscribe-modal-open]'),
+  closeModalBtn: document.querySelector('[data-subscribe-modal-close]'),
+  modal: document.querySelector('[data-subscribe-modal]'),
+};
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+function toggleModal(event) {
+  refs.modal.classList.toggle('is-hidden');
+}
