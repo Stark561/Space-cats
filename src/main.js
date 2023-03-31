c864cc0b7838e680fbbcf47280880df327321893
Vladislavv99 <vladlegur6@gmail.com>
2023-03-31 23:17:50 +0300

    готовые секции

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './js/swiper';
 import './js/modal-sub';
-import "./js/leave-a-review"
+import './js/leave-a-review';
 import './js/buy-now-modal';
 import './js/modal-orange';
 import './js/modal-orange-second';
+import './js/smooth-scroll';
+import './js/form-sub';
