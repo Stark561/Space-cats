e997ca35509b294ef7c7b4b0f2446b013341d8e8
m1lky4 <temanafanin1337@gmail.com>
2023-04-01 16:51:20 +0200

    changed sasha's js

--- src/js/leave-a-review.js
+++ src/js/leave-a-review.js
@@ -1,15 +1,17 @@
+
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[review-modal-open]"),
-      closeModalBtn: document.querySelector("[review-modal-close]"),
-      modal: document.querySelector("[review-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal(event) {
-      event.preventDefault ();
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector('[review-modal-open]'),
+    closeModalBtn: document.querySelector('[review-modal-close]'),
+    modal: document.querySelector('[review-modal]'),
+    body: document.querySelector('[data-body]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
