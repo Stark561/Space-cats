10a7cdfea70a677049f70923656d8980b59bcd6a
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-04-01 18:45:11 +0300

    Закончил Работу

--- src/js/buy-now-modal.js
+++ src/js/buy-now-modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[buy-now-modal-open]'),
     closeModalBtn: document.querySelector('[buy-now-modal-close]'),
     modal: document.querySelector('[buy-now-modal]'),
+    body: document.querySelector('[data-body]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
